d0e69934e14db70f655f1efd35a17e52b7aa43e1
Peixinho <joanapeixinho0@gmail.com>
2023-06-11 00:49:14 +0100

    add customers view

--- app/app.py
+++ app/app.py
@@ -16,8 +16,11 @@ from psycopg_pool import ConnectionPool
 # postgres://{user}:{password}@{hostname}:{port}/{database-name}
 DATABASE_URL = "postgres://db:db@postgres/db"
 
+app = Flask(__name__, static_url_path='/static')
+
 pool = ConnectionPool(conninfo=DATABASE_URL)
 # the pool starts connecting immediately.
+
 
 dictConfig(
     {
@@ -43,22 +46,81 @@ log = app.logger
 
 
 @app.route("/test", methods=("GET",))
-def index():
+def test():
     """Show the index page."""
 
     return render_template("test.html")
 
-@app.route("/accounts", methods=("GET",))
-def account_index():
+@app.route('/create-customer', methods=['POST'])
+def create_customer():
+    # Obtenha os dados do formulário enviado
+    name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    address = request.form.get('address')
+
+  
+
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute("SELECT MAX(cust_no) FROM CUSTOMER")
+            max_cust_no = cur.fetchone()[0]
+            cust_no = max_cust_no + 1
+            cur.execute("INSERT INTO customer (cust_no, name, email, phone, address) VALUES (%(cust_no)s, %(name)s, %(email)s, %(phone)s, %(address)s)", {"cust_no": cust_no, "name": name, "email": email, "phone": phone, "address": address})
+            log.debug(f"Inserted {cur.rowcount} rows.")
+    return redirect('/customers')
+
+@app.route("/", methods=("GET",))
+def homepage():
+    """Show the index page."""
+
+    return render_template("homepage.html", current_page="homepage")
+
+@app.route("/products", methods=("GET",))
+def products():
+    """Show the index page."""
+
+    return render_template("homepage.html", current_page="products")
+
+
+@app.route("/orders", methods=("GET",))
+def orders():
+    """Show the index page."""
+
+    
+    return render_template("homepage.html", current_page="orders")
+
+@app.route('/delete-customer', methods=['POST'])
+
+def delete_customer():
+    cust_no = request.form['cust_no']
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute("DELETE FROM contains WHERE order_no IN (SELECT order_no FROM orders WHERE cust_no = %(cust_no)s)", {"cust_no": cust_no})
+            cur.execute("DELETE FROM pay WHERE order_no IN (SELECT order_no FROM orders WHERE cust_no = %(cust_no)s)", {"cust_no": cust_no})
+            cur.execute("DELETE FROM process WHERE order_no IN (SELECT order_no FROM orders WHERE cust_no = %(cust_no)s)", {"cust_no": cust_no})
+            cur.execute("DELETE FROM orders WHERE cust_no = %(cust_no)s", {"cust_no": cust_no})
+            cur.execute("DELETE FROM customer WHERE cust_no = %(cust_no)s", {"cust_no": cust_no})
+            
+
+            log.debug(f"Deleted {cur.rowcount} rows.")
+    
+    
+    return redirect('/customers')
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route("/customers", methods=("GET",))
+def customers():
     """Show all the accounts, most recent first."""
 
     with pool.connection() as conn:
         cur = conn.cursor(row_factory=namedtuple_row)
         cur.execute(
                 """
-                SELECT account_number, branch_name, balance
-                FROM account
-                ORDER BY account_number DESC;
+                SELECT * FROM customer;
                 """,
                 {},
             )
@@ -71,7 +133,7 @@ def account_index():
     ):
         return jsonify(cur)
 
-    return render_template("account/index.html", accounts=cur)
+    return render_template("customer/customers.html", customers=cur, current_page="customers")
 
 
 
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // or have default horizontal padding
+      padding: '1rem',
+
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1496px',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('flowbite/plugin')
   ],
 }
 
