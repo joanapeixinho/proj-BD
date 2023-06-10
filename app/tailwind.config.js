7ef6e9d5770a45f1b1efa055cd005f0a8c1f8854
Peixinho <joanapeixinho0@gmail.com>
2023-06-10 15:56:19 +0100

    update tailwind

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./node_modules/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
 }
 
